d3a131c2b2cc7658c87e2ea6d69e8d6fc27d4bd1
uofkengineering <uofkengineering@gmail.com>
2025-10-19 23:47:36 +0100

    Create script.js

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+(function(){
+const y=document.getElementById('year');
+if(y) y.textContent=new Date().getFullYear();
+
+
+// تمكين تمرير رابط نموذج Google عبر معلمة ?form=...
+const frame=document.getElementById('gform');
+const a=document.getElementById('gform-link');
+const params=new URLSearchParams(location.search);
+const url=params.get('form')||'';
+if(frame && url){
+frame.src=url;
+if(a) a.href=url;
+}
+})();
